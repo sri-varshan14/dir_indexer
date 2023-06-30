503691588ce90f5e13a4eecde6645313e2578db5
sri-varshan14 <mailmeatsri14@gmail.com>
2023-06-30 12:49:29 +0530

    Added basic documentation

--- tests/sample.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-use dir_indexer::DirIndexer;
-use std::any::TypeId;
-
-#[test]
-fn test_creating_dir_indexer_obj() {
-    let a = DirIndexer::from(PathBuf::from("source"));
-    assert_ne!(a.is_some(), true);
-}
